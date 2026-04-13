cf2e7a7303852304ae5e449926fae932eea9fc7b
vova7111 <vova7111@gmail.com>
2026-04-12 22:30:35 -0300

    add Enemy Class

---
+++ RogueLib/Dungeon/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using RogueLib.Utilities;
+using System;
+
+using System.Text;
+
+namespace RogueLib.Dungeon
+{
+    public abstract class Enemy : IActor, IDrawable
+    {
+        public Vector2 Pos { get; set; }
+
+        public char Glyph { get; protected init; }
+
+        public ConsoleColor Color { get; protected init; }
+
+        protected int _hp;
+        protected int _attack;
+        protected int _turnCounter;
+
+        protected Enemy(Vector2 pos, char glyph, ConsoleColor color, int hp, int attack)
+        {
+            Pos = pos;
+            Glyph = glyph;
+            Color = color;
+            _hp = hp;
+            _attack = attack;
+        }
+
+        public abstract void Act(Vector2 playerPos, HashSet<Vector2> walkable);
+        public abstract void Draw(IRenderWindow disp);
+
+        public virtual void Update() => _turnCounter++;
+    }
+}
---
+++ RougeLikeGame/Actors/Goblin.cs
@@ -0,0 +1,34 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+
+
+namespace SandBox01.Actors
+{
+    public class Goblin : Enemy
+    {
+
+        private static readonly Random _rng = new Random();
+
+        private static readonly Vector2[] _directions =
+        {
+            Vector2.N, Vector2.S, Vector2.E, Vector2.W
+        };
+
+        public Goblin(Vector2 pos) : base(pos, 'g', ConsoleColor.Green, 6, 3) { }
+
+        public override void Act(Vector2 playerPos, HashSet<Vector2> walkable)
+        {
+            var dir = _directions[_rng.Next(_directions.Length)];
+            var newPos = Pos + dir;
+
+            if (walkable.Contains(newPos))
+                Pos = newPos;
+        }
+
+
+        public override void Draw(IRenderWindow disp)
+        {
+            disp.Draw(Glyph, Pos, Color);
+        }
+    }
+}
